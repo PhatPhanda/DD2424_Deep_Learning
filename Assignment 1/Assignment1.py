9706f91f6a0edb02acb42858e039fd092d5550b9
PhatPhanda <simonyu15@gmail.com>
2026-03-26 17:02:06 +0100

    Remove dataset and add gitignore

---
+++ Assignment 1/Assignment1.py
@@ -0,0 +1,12 @@
+import pickle 
+import numpy as np
+
+def load_batch(filename):
+    cifar_dir = 'Assignment 1/Datasets/cifar-10-batches-py/'
+    with open(cifar_dir + filename, 'rb') as fo:
+        dict = pickle.load(fo, encoding='bytes')
+    X = (dict[b'data'].astype(np.float64) / 255.0).T
+    y = dict[b'labels']
+    nn = X.shape[1]    
+
+load_batch('data_batch_1')
---
+++ Assignment 1/Test.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pickle
+import os
+
+
+
+# Load a batch of training data
+cifar_dir = 'Assignment 1/Datasets/cifar-10-batches-py/'
+print(os.path.exists(cifar_dir))
+with open(cifar_dir + 'data_batch_1', 'rb') as fo:
+    dict = pickle.load(fo, encoding='bytes')
+# Extract the image data and cast to float from the dict dictionary
+X = dict[b'data'].astype(np.float64) / 255.0
+X = X.transpose()
+nn = X.shape[1]
+# Reshape each image from a column vector to a 3d array
+X_im = X.reshape((32, 32, 3, nn), order='F')
+X_im = np.transpose(X_im, (1, 0, 2, 3))
+4
+# Display the first 5 images
+ni = 5
+fig, axs = plt.subplots(1, 5, figsize=(10, 5))
+for i in range(ni):
+    axs[i].imshow(X_im[:, :, :, i])
+    axs[i].axis('off')
+plt.pause(2)
+
