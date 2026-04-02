6e627f3b5a4e09d8914042615e7772a0301af88a
PhatPhanda <simonyu15@gmail.com>
2026-04-02 17:14:10 +0200

    funtions done

--- Assignment 1/Assignment1.py
+++ Assignment 1/Assignment1.py
@@ -1,6 +1,7 @@
 import pickle 
 import numpy as np
 from torch_gradient_computations import ComputeGradsWithTorch
+import copy
 
 def load_batch(filename):
     cifar_dir = 'Assignment 1/Datasets/cifar-10-batches-py/'
@@ -34,7 +35,7 @@ def init_parameters(K, d):
     init_net['W'] = .01*rng.standard_normal(size = (K, d))
     init_net['b'] = np.zeros((K, 1))
 
-    return init_net
+    return init_net, rng
 
 
 
@@ -67,6 +68,12 @@ def backward_pass(X, Y, P, network, lam):
     grad['b'] = np.sum(G, axis=1, keepdims=True) / n
     return grad
 
+def compute_cost(P, y, network, lam):
+    loss = compute_loss(P, y)
+    reg = lam * np.sum(network['W'] ** 2)
+    cost = loss + reg
+    return cost
+
     
 def compare_grads(my_grads, torch_grads):
     for key in ['W', 'b']:
@@ -85,7 +92,43 @@ def compare_grads(my_grads, torch_grads):
         print("max absolute difference:", max_abs_diff)
         print("max relative error     :", rel_error)
 
-def main():
+
+def mini_batch_GD(X, Y, y, GD_params, init_net, lam, rng):
+    trained_net = copy.deepcopy(init_net)
+    n = X.shape[1]
+    n_batch = GD_params['n_batch']
+    eta = GD_params['eta']
+    n_epochs = GD_params['n_epochs']
+
+    train_costs = []
+
+    for epoch in range(n_epochs):
+        perm = rng.permutation(n)
+        X_shuffled = X[:, perm]
+        Y_shuffled = Y[:, perm]
+    
+
+        for j in range(n // n_batch):
+            j_start = j * n_batch
+            j_end = (j+1)*n_batch
+            Xbatch = X_shuffled[:, j_start:j_end]
+            Ybatch = Y_shuffled[:, j_start:j_end]
+
+            P_batch = apply_network(Xbatch, trained_net)
+            grads = backward_pass(Xbatch, Ybatch, P_batch, trained_net, lam)
+
+            trained_net['W'] -= eta * grads['W']
+            trained_net['b'] -= eta * grads['b']
+        
+        P_train = apply_network(X, trained_net)
+        train_cost = compute_cost(P_train, y, trained_net, lam)
+        train_costs.append(train_cost)
+
+        print(f"Epoch {epoch+1}/{n_epochs}, training cost: {train_cost:.6f}")
+
+    return trained_net, train_costs
+
+def test_gradients():
     X_train, Y_train, y_train = load_batch('data_batch_1')
     X_val, Y_val, y_val = load_batch('data_batch_2') 
     X_test, Y_test, y_test = load_batch('data_batch_3')
@@ -112,15 +155,28 @@ def main():
     compare_grads(my_grads, torch_grads)
 
 
+
+def main():
+    X_train, Y_train, y_train = load_batch('data_batch_1')
+    X_val, Y_val, y_val = load_batch('data_batch_2') 
+    X_test, Y_test, y_test = load_batch('data_batch_3')
+
+    X_mean, X_std = compute_stats(X_train)
+    X_train = normalize(X_train, X_mean, X_std)
+    X_val = normalize(X_val, X_mean, X_std)
+    X_test = normalize(X_test, X_mean, X_std)
+    d = X_train.shape[0]
+    K = Y_train.shape[0]
+    
+    init_net, rng = init_parameters(K, d)
+    GD_params = {
+    'n_batch': 100,
+    'eta': 0.001,
+    'n_epochs': 40
+    }
+    trained_net, train_costs = mini_batch_GD(X_train, Y_train, y_train, GD_params, init_net, 0, rng)
     
 
-
-
 main()
-
-
-
-
-
 
 
