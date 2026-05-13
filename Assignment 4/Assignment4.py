a7b720022f1f2d869aa559ef93d303490fbe4d34
PhatPhanda <simonyu15@gmail.com>
2026-05-13 16:50:59 +0200

    <zxc<zxc

--- Assignment 4/Assignment4.py
+++ Assignment 4/Assignment4.py
@@ -26,11 +26,12 @@ def init_parameters(K, m):
     seed = 42
 
     RNN = {'b': np.zeros((m,1)),
-           'c': np.zeros((K,1))
+           'c': np.zeros((K,1)),
+           'U': (1/np.sqrt(2*K))*rng.standard_normal(size = (m, K)),
+           'W': (1/np.sqrt(2*m))*rng.standard_normal(size = (m, m)),
+           'V': (1/np.sqrt(m))*rng.standard_normal(size = (K, m))      
            }
-    RNN['U'] = (1/np.sqrt(2*K))*rng.standard_normal(size = (m, K))
-    RNN['W'] = (1/np.sqrt(2*m))*rng.standard_normal(size = (m, m))
-    RNN['V'] = (1/np.sqrt(m))*rng.standard_normal(size = (K, m))
+
 
     return RNN, rng
 
@@ -78,6 +79,77 @@ def forward_pass(RNN, X, Y, h0):
         h[:, t:t+1] = np.tanh(a[:, t:t+1])
         o[:, t:t+1] = RNN["V"] @ h[:, t:t+1] + RNN["c"]
         p[:, t:t+1] = np.exp(o) / np.sum(np.exp(o), axis=0, keepdims=True)
+
+
+        loss += -(y_t.T @ np.log(p[:, t:t+1]))[0,0]
+        h_prev = h
+
+    loss = loss / seq_length
+
+    fp_data = {
+        "X": X,
+        "Y": Y,
+        "a": a,
+        "h": h,
+        "o": o,
+        "p": p,
+        "h0": h0
+    }
+
+    return loss, fp_data
+
+
+def backward_pass(RNN, fp_data):
+    b = RNN['b']
+    c = RNN['c']
+    W = RNN['W']
+    V = RNN['V']
+    U = RNN['U']
+
+    X = fp_data['X']
+    Y = fp_data['Y']
+    a = fp_data['a']
+    h = fp_data['h']
+    o = fp_data['o']
+    p = fp_data['p']
+
+    seq_length = X.shape[1]
+    dh_next = np.zeros_like(h)
+
+
+    # init grads
+    grads = {}
+
+    grads["U"] = np.zeros_like(RNN["U"])
+    grads["W"] = np.zeros_like(RNN["W"])
+    grads["V"] = np.zeros_like(RNN["V"])
+    grads["b"] = np.zeros_like(RNN["b"])
+    grads["c"] = np.zeros_like(RNN["c"])
+
+    
+    for t in reversed(range(seq_length)):
+        x_t = X[:, t:t+1]
+        y_t = Y[:, t:t+1]
+        p_t = p[:, t:t+1]
+        h_t = h[:, t:t+1]
+        a_t = a[:, t:t+1]
+        
+
+        g_t = -(y_t - p_t).T
+
+        grads['V'] += g_t.T @ h_t.T
+        grads['c'] += g_t
+
+        dh = g_t @ V + dh_next
+
+        da = dh @ np.diag(1 - np.tanh(a_t)**2)
+
+        grads['W'] += da.T @ h[:, t-1:t].T
+        grads['U'] += da.T @ x_t.T
+        grads['b'] += da
+
+        dh_next = da @ RNN['W']
+
 
 
 
